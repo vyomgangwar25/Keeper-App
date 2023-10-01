de7479a25706dcfa619ef5961a3d98fa350770b3
vyomgangwar25 <vyomgangwar25@gmail.com>
2023-10-01 11:49:38 +0530

    create a createArea component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import CreateArea from "./Components/CreateArea";
 import "./Style.css";
 function App() {
   return (
     <div className="App">
        <Header/>
        <Footer/>
+       <CreateArea/>
     </div>
   );
 }
---
+++ src/Components/CreateArea.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const CreateArea = () => {
+  return (
+    <div>
+      <form action="">
+        <input type="text" placeholder='Title' />
+        <textarea name="" id="" cols="" rows="" placeholder='Write a note...'></textarea>
+         
+      </form>
+    </div>
+  )
+}
+
+export default CreateArea
